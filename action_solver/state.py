de403098f4588e814c5821ce5e24f94ca799f207
Florian Schieder <florian.schieder@web.de>
2025-07-06 20:37:10 +0200

    turned SolverState into plain class

--- action_solver/state.py
+++ action_solver/state.py
@@ -1,23 +1,27 @@
-from dataclasses import dataclass
 from typing import Any, Self
 
 from .action import Action
 
 
-@dataclass(frozen=True)
 class SolverState:
     """
     State wrapper for the lifetime of a :class:`ActionSolver`.
-
-    Attributes:
-    - `globals`: Global variables which are relevant for some actions
-      and which are all known before any action has been started.
-    - `results`: This is where the :class:`ActionSolver` stores a result
-      if a :class:`Action` implementation returned one.
     """
 
-    globals: dict[str, Any]
-    results: dict[type[Action.Result], Action.Result]
+    def __init__(
+        self,
+        globals: dict[str, Any],
+        results: dict[type[Action.Result], Action.Result],
+    ):
+        """
+        TODO PARAMETERS
+        - `globals`: Global variables which are relevant for some actions
+        and which are all known before any action has been started.
+        - `results`: This is where the :class:`ActionSolver` stores a result
+        if a :class:`Action` implementation returned one.
+      """
+        self._globals = globals
+        self._results = results
 
     @classmethod
     def construct_empty(cls) -> Self:
@@ -26,7 +30,7 @@ class SolverState:
     def get_result[TResult: Action.Result](
         self, result_type: type[TResult]
     ) -> TResult:
-        result_candidate = self.results[result_type]
+        result_candidate = self._results[result_type]
         if not isinstance(result_candidate, result_type):
             raise TypeError(
                 f"lookup on result type '{result_type.__name__}' "
